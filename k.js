2710e3639e4e4b7dce622acb4e57b11c74d6ee89
yumiyulili <masha.ava04@gmail.com>
2025-04-07 22:24:50 +0300

    .

---
+++ k.js
@@ -0,0 +1,50 @@
+function addNumbers() {
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
+
+  let num;
+  if (num1.toString() == "NaN") {
+    alert("error");
+    return;
+  } else if (num2.toString() == "NaN") {
+    alert("error");
+    return;
+  }
+
+  const symbol = document.getElementById("symbol").value;
+
+  let result;
+
+  if (symbol == "*") {
+
+    result = num1 * num2;
+
+  } else if (symbol == "+") {
+
+    result = num1 + num2;
+
+  } else if (symbol == "/") {
+    if (num2 == 0) {
+      alert("error");
+      return;
+    }
+
+    result = num1 / num2;
+
+  } else if (symbol == "-") {
+
+    result = num1 - num2;
+    
+  } else {
+    alert("error");
+  }
+
+  if (result == undefined) {
+    return;
+  }
+
+  document.getElementById("result").innerText = "Result = " + result;
+}
